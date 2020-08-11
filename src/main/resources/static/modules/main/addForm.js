e34cc4d43ab3b724a804e0d5ef2506236350122c
zhaoershuai <zhaoershuai@sinopharmholding-fl.com>
2020-08-11 20:01:48 +0800

    'init'

---
+++ src/main/resources/static/modules/main/addForm.js
@@ -0,0 +1,11 @@
+layui.define(['layer', 'form','jquery'], function(exports){
+    let $ = layui.jquery;
+    let layer = layui.layer;
+    layer.open({
+        title: '在线调试',
+        content: 'aaa'
+    });
+    exports('addForm', {});
+});
+
+
